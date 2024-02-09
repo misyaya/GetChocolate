ba0b04291f147897580ad8bb3c5bff9490bc0c65
彩姫 三澤 <220285@jc-21.jp>
2024-02-09 10:10:30 +0900

    コライダーの消し方が分かった

--- Sword.cpp
+++ Sword.cpp
@@ -33,10 +33,9 @@ void Sword::Initialize()
 void Sword::Update()
 {
     //ÉGÉìÉ^Å[Ç™âüÇ≥ÇÍÇΩÇÁ
-    if (Input::IsKeyDown(DIK_RIGHT) )
+    if (Input::IsKeyDown(DIK_RETURN) )
     {
-        BoxCollider* collision = new BoxCollider(XMFLOAT3(0, 0, 0), XMFLOAT3(1, 1, 1));
-        AddCollider(collision);
+        ClearCollider();
     }
 
    
@@ -45,6 +44,7 @@ void Sword::Update()
     {
         BoxCollider* collision = new BoxCollider(XMFLOAT3(0, 1, 1), XMFLOAT3(1, 1, 1));
         AddCollider(collision);
+        
     }
 
     if (Input::IsKey(DIK_W))
